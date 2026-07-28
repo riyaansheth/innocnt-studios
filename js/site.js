80cb23390050b37ff85ca90a0fcc56d2513d0903
Riyaan Amit Sheth <riyaansheth@gmail.com>
2026-07-28 16:59:00 +0530

    Refine World headings and collection spacing

--- js/site.js
+++ js/site.js
@@ -248,7 +248,7 @@ const worldMain = document.querySelector('#main');
 if (worldMain) {
   const worldStyles = document.createElement('link');
   worldStyles.rel = 'stylesheet';
-  worldStyles.href = new URL('../css/world-section-layout.css', window.location.href);
+  worldStyles.href = new URL('../css/world-section-layout.css?revision=world-display-headings-20260728', window.location.href);
   document.head.append(worldStyles);
 
   worldMain.querySelectorAll('.section-head').forEach((heading) => {
